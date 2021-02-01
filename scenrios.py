334d91dbc9a5c65d46a722d6eac46b1aacaa545d
Kyeongwon <aelleek@gmail.com>
2021-02-01 17:01:17 +0900

    Init Sprinle Project

---
+++ scenrios.py
@@ -0,0 +1,29 @@
+"""
+  application.py
+  Sprinkle
+
+  Created by LeeKW on 2021/02/01.
+"""
+
+from kochat.app import Scenario
+from do import do_call, do_schedule
+
+
+call = Scenario(
+    intent='call',
+    api=do_call,
+    scenario={
+        'Target': [],
+    }
+)
+
+schedule = Scenario(
+    intent='schedule',
+    api=do_schedule,
+    scenario={
+        'Date':[],
+        'Subject':[],
+        'Time':[],
+        'Action':[],
+    }
+)
