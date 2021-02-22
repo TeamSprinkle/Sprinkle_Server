42a06d40c4942d80e89af47ce99650a1d9c26cd0
Kyeongwon <aelleek@gmail.com>
2021-02-22 14:28:49 +0900

    Update application.py

--- application.py
+++ application.py
@@ -5,30 +5,64 @@
   Created by LeeKW on 2021/02/01.
 """
 
-from flask import Flask, request
+from flask import request
 
+from kochat.app import KochatApi
+from kochat.data import Dataset
+from kochat.loss import CRFLoss, CosFace, CenterLoss, COCOLoss, CrossEntropyLoss
+from kochat.model import intent, embed, entity
+from kochat.proc import DistanceClassifier, GensimEmbedder, EntityRecognizer, SoftmaxClassifier
+
+from sprinkle.scenrios import call, schedule
 from sprinkle.controllers.UserController import UserController
 from sprinkle.controllers.VoiceCommandController import VoiceCommandController
 
 
+dataset = Dataset(ood=True)
+emb = GensimEmbedder(model=embed.FastText())
+
+clf = DistanceClassifier(
+    model=intent.CNN(dataset.intent_dict),
+    loss=CenterLoss(dataset.intent_dict),
+)
+
+rcn = EntityRecognizer(
+    model=entity.LSTM(dataset.entity_dict),
+    loss=CRFLoss(dataset.entity_dict)
+)
+
+kochat = KochatApi(
+    dataset=dataset,
+    embed_processor=(emb, False),
+    intent_classifier=(clf, False),
+    entity_recognizer=(rcn, False),
+    scenarios=[
+        call, schedule
+    ]
+)
+
 userController = UserController()
 voiceCommandControler = VoiceCommandController()
 
-@app.route('/')
+@kochat.app.route('/')
 def index():
     # userController.searchUserById("1234567890")
     return "adsf"
 
-@app.route('/users/init', methods=['POST'])
+@kochat.app.route('/users/init', methods=['POST'])
 def init():
     userInfo = request.json
     return userController.createUser(userInfo)
 
-@app.route('/command/test', methods=['GET'])
+@kochat.app.route('/command/test', methods=['GET'])
 def test():
-    return voiceCommandControler.test("천우한테 전화 걸어줘")
+    return voiceCommandControler.test("천우에게 전화 걸어줘")
+
+
+# @kochat.app.route('/getUsers')
+# def getUsers():
+#     return userController.searchUser()
+
 
 if __name__ == '__main__':
-    app = Flask(__name__)
-    app.run(port=8080, host='0.0.0.0')
-
+    kochat.app.run(port=8080, host='0.0.0.0')
