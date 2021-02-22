7effe1851e44b6f1eab8ffb11b871eb823b036e0
Kyeongwon <aelleek@gmail.com>
2021-02-22 14:04:40 +0900

    Add Voice Command Test code

--- application.py
+++ application.py
@@ -5,56 +5,30 @@
   Created by LeeKW on 2021/02/01.
 """
 
-from flask import request
+from flask import Flask, request
 
-from kochat.app import KochatApi
-from kochat.data import Dataset
-from kochat.loss import CRFLoss, CosFace, CenterLoss, COCOLoss, CrossEntropyLoss
-from kochat.model import intent, embed, entity
-from kochat.proc import DistanceClassifier, GensimEmbedder, EntityRecognizer, SoftmaxClassifier
-
-from sprinkle.scenrios import call, schedule
 from sprinkle.controllers.UserController import UserController
+from sprinkle.controllers.VoiceCommandController import VoiceCommandController
 
-dataset = Dataset(ood=True)
-emb = GensimEmbedder(model=embed.FastText())
-
-clf = DistanceClassifier(
-    model=intent.CNN(dataset.intent_dict),
-    loss=CenterLoss(dataset.intent_dict),
-)
-
-rcn = EntityRecognizer(
-    model=entity.LSTM(dataset.entity_dict),
-    loss=CRFLoss(dataset.entity_dict)
-)
-
-kochat = KochatApi(
-    dataset=dataset,
-    embed_processor=(emb, False),
-    intent_classifier=(clf, False),
-    entity_recognizer=(rcn, False),
-    scenarios=[
-        call, schedule
-    ]
-)
 
 userController = UserController()
+voiceCommandControler = VoiceCommandController()
 
-@kochat.app.route('/')
+@app.route('/')
 def index():
     # userController.searchUserById("1234567890")
     return "adsf"
 
-@kochat.app.route('/users/init', methods=['POST'])
+@app.route('/users/init', methods=['POST'])
 def init():
     userInfo = request.json
     return userController.createUser(userInfo)
 
-# @kochat.app.route('/getUsers')
-# def getUsers():
-#     return userController.searchUser()
-
+@app.route('/command/test', methods=['GET'])
+def test():
+    return voiceCommandControler.test("천우한테 전화 걸어줘")
 
 if __name__ == '__main__':
-    kochat.app.run(port=8080, host='0.0.0.0')
+    app = Flask(__name__)
+    app.run(port=8080, host='0.0.0.0')
+
--- sprinkle/controllers/VoiceCommandController.py
+++ sprinkle/controllers/VoiceCommandController.py
@@ -5,10 +5,52 @@
   Created by LeeKW on 2021/02/19.
 """
 
-from sprinkle.models.VoiceCommand import User
+# from sprinkle.models.VoiceCommand import User
+from kochat.app import KochatApi
+from kochat.app import KochatApi
+from kochat.data import Dataset
+from kochat.loss import CRFLoss, CosFace, CenterLoss, COCOLoss, CrossEntropyLoss
+from kochat.model import intent, embed, entity
+from kochat.proc import DistanceClassifier, GensimEmbedder, EntityRecognizer, SoftmaxClassifier
 
-
+from sprinkle.scenrios import call, schedule
 class VoiceCommandController():
     def __init__(self):
-        self.voiceCommand = User()
+        # self.voiceCommand = User()
+        dataset = Dataset(ood=True)
+        emb = GensimEmbedder(model=embed.FastText())
+
+        clf = DistanceClassifier(
+            model=intent.CNN(dataset.intent_dict),
+            loss=CenterLoss(dataset.intent_dict),
+        )
+
+        rcn = EntityRecognizer(
+            model=entity.LSTM(dataset.entity_dict),
+            loss=CRFLoss(dataset.entity_dict)
+        )
+
+        self.kochat = KochatApi(
+            dataset=dataset,
+            embed_processor=(emb, False),
+            intent_classifier=(clf, False),
+            entity_recognizer=(rcn, False),
+            scenarios=[
+                call, schedule
+            ]
+        )
+
+    def test(self, text):
+    
+        prep = self.kochat.dataset.load_predict(text, self.kochat.embed_processor)
+        intent = self.kochat.intent_classifier.predict(prep, calibrate=False)
+        entity = self.kochat.entity_recognizer.predict(prep)
+        text = self.kochat.dataset.prep.tokenize(text, train=False)
+        # KochatApi.dialogue_cache[uid] = KochatApi.scenario_manager.apply_scenario(intent, entity, text)
+        
+        print(intent)
+        print(entity)
+        print(text)
+
+        return "aa"
     
