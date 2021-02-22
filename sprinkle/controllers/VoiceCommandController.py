74211e4c4c1bfca97ec7a8b5a14f6c272a6b19ff
Kyeongwon <aelleek@gmail.com>
2021-02-22 14:54:35 +0900

    Update VoiceCommandController.py init def

--- sprinkle/controllers/VoiceCommandController.py
+++ sprinkle/controllers/VoiceCommandController.py
@@ -14,38 +14,41 @@ from kochat.model import intent, embed, entity
 from kochat.proc import DistanceClassifier, GensimEmbedder, EntityRecognizer, SoftmaxClassifier
 
 from sprinkle.scenrios import call, schedule
+
+dataset = Dataset(ood=True)
+emb = GensimEmbedder(model=embed.FastText())
+
+clf = DistanceClassifier(
+    model=intent.CNN(dataset.intent_dict),
+    loss=CenterLoss(dataset.intent_dict),
+)
+
+rcn = EntityRecognizer(
+    model=entity.LSTM(dataset.entity_dict),
+    loss=CRFLoss(dataset.entity_dict)
+)
+
+kochat = KochatApi(
+    dataset=dataset,
+    embed_processor=(emb, False),
+    intent_classifier=(clf, False),
+    entity_recognizer=(rcn, False),
+    scenarios=[
+        call, schedule
+    ]
+)
+
 class VoiceCommandController():
     def __init__(self):
         # self.voiceCommand = User()
-        dataset = Dataset(ood=True)
-        emb = GensimEmbedder(model=embed.FastText())
-
-        clf = DistanceClassifier(
-            model=intent.CNN(dataset.intent_dict),
-            loss=CenterLoss(dataset.intent_dict),
-        )
-
-        rcn = EntityRecognizer(
-            model=entity.LSTM(dataset.entity_dict),
-            loss=CRFLoss(dataset.entity_dict)
-        )
-
-        self.kochat = KochatApi(
-            dataset=dataset,
-            embed_processor=(emb, False),
-            intent_classifier=(clf, False),
-            entity_recognizer=(rcn, False),
-            scenarios=[
-                call, schedule
-            ]
-        )
+        print("")
 
     def test(self, text):
     
-        prep = self.kochat.dataset.load_predict(text, self.kochat.embed_processor)
-        intent = self.kochat.intent_classifier.predict(prep, calibrate=False)
-        entity = self.kochat.entity_recognizer.predict(prep)
-        text = self.kochat.dataset.prep.tokenize(text, train=False)
+        prep = kochat.dataset.load_predict(text, kochat.embed_processor)
+        intent = kochat.intent_classifier.predict(prep, calibrate=False)
+        entity = kochat.entity_recognizer.predict(prep)
+        text = kochat.dataset.prep.tokenize(text, train=False)
         # KochatApi.dialogue_cache[uid] = KochatApi.scenario_manager.apply_scenario(intent, entity, text)
         
         print(intent)
