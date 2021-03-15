60d76e3bcacaad01feee50ea5f336ac8e557aef3
Kyeongwon <aelleek@gmail.com>
2021-03-16 04:13:57 +0900

    Update application

--- application.py
+++ application.py
@@ -51,7 +51,7 @@ kochat = KochatApi(
 
 @kochat.app.route('/users/init', methods=['POST'])
 def init():
-    userInfo = request.form.get("")
+    userInfo = request.form.get().to_dict()
     print("userInfo")
     print(userInfo)
     return userController.createUser(userInfo)
